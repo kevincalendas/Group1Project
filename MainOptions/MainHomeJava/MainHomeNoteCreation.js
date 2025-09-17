f884b75af1b73d82f5bc26a1fcc112bb81aff68a
KevinCalendas <johnkevixcalendas@gmail.com>
2025-09-17 19:11:09 +0800

    changes: removing

---
+++ MainOptions/MainHomeJava/MainHomeNoteCreation.js
@@ -0,0 +1,9 @@
+function NoteOpenCreationSetup() {
+    const MainButtonNoteCreationOpen = document.getElementById('MainButtonCreateFileExecute');
+    const MainWindowOpen = document.getElementById('NoteCreationWindowSetup');
+    const NoteNameCreationSetup1A = document.getElementById('NoteInputHeading1');
+        MainWindowOpen.classList.toggle('OpenNoteCodeCreation');
+        NoteNameCreationSetup1A.classList.toggle('OpenNoteSlideAnimation');
+    
+    
+}
