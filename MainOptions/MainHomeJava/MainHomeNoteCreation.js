8fa66ae112a907ccb7d656b0e1cde5f790a6cc22
kevincalendas <johnkevixcalendas@gmail.com>
2025-10-01 20:31:39 +0800

    changes

--- MainOptions/MainHomeJava/MainHomeNoteCreation.js
+++ MainOptions/MainHomeJava/MainHomeNoteCreation.js
@@ -1,15 +1,5 @@
-function NoteOpenCreationSetup() {
-    const MainButtonNoteCreationOpen = document.getElementById('MainButtonCreateFileExecute');
-    const MainWindowOpen = document.getElementById('NoteCreationWindowSetup');
-    const NoteNameCreationSetup1A = document.getElementById('NoteInputHeading1');
-        MainButtonNoteCreationOpen.addEventListener("click", function () {
-            if (MainWindowOpen.classList.contains('OpenNoteCodeCreation-Hide')) {
-                MainWindowOpen.classList.remove('OpenNoteCodeCreation-Hide');
-                MainWindowOpen.classList.add('OpenNoteCodeCreation-Open');
-            } else {
-                MainWindowOpen.classList.add('OpenNoteCodeCreation-Hide');
-                MainWindowOpen.classList.remove('OpenNoteCodeCreation-Open');
-                void MainWindowOpen.offsetWidth;
-            }
-        });
+function OpenCreationSETUP {
+    const notewindowopen = document.getElementById('NoteCreationWindowSetup');
+    const notewindowicon = document.getElementById('NoteCreationWindowSetup');
+    notewindowopen.style.transform = transformY
 }
